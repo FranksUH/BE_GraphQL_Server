9515f86d4f819803967452299884c00716a3950d
Frankie Mujica <frankie.mujica@wundermanthompson.com>
2022-09-30 12:11:29 +0200

    code cleanup

--- GraphQL_Example/Program.cs
+++ GraphQL_Example/Program.cs
@@ -1,29 +1,7 @@
-using DAL;
-using DAL.Definitions;
-using DAL.Implementations;
-using Microsoft.EntityFrameworkCore;
+using GraphQL_Example;
 
 var builder = WebApplication.CreateBuilder(args);
-ConfigurationManager configuration = builder.Configuration;
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Register custom services for the superheroes
-//builder.Services.AddScoped<ISuperheroRepository, SuperheroRepository>();
-//builder.Services.AddScoped<ISuperpowerRepository, SuperpowerRepository>();
-//builder.Services.AddScoped<IMovieRepository, MovieRepository>();
-builder.Services.AddGraphQLServer()
-                .AddQueryType<Query>()
-                .AddProjections()
-                .AddSorting()
-                .AddFiltering();
-
-// Add Application Db Context options
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(configuration.GetConnectionString("SqlServer")));
+builder.AddRequiredServices();
 
 var app = builder.Build();
 
@@ -35,30 +13,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.MapGraphQL("/graphql");
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ GraphQL_Example/ServicesConfiguration.cs
@@ -0,0 +1,31 @@
+﻿namespace GraphQL_Example
+{
+    using DAL;
+    using Microsoft.EntityFrameworkCore;
+
+    public static class ServicesConfiguration
+    {
+        public static void AddRequiredServices(this WebApplicationBuilder builder)
+        {
+            // Add services to the container.
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            // Register Repos
+            //builder.Services.AddScoped<ISuperheroRepository, SuperheroRepository>();
+            //builder.Services.AddScoped<ISuperpowerRepository, SuperpowerRepository>();
+            //builder.Services.AddScoped<IMovieRepository, MovieRepository>();
+
+            // Register GraphQL
+            builder.Services.AddGraphQLServer()
+                            .AddQueryType<Query>()
+                            .AddProjections()
+                            .AddSorting()
+                            .AddFiltering();
+
+            // Add Application Db Context options
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+        }
+    }
+}
